7581154a0ccca63f49e3cf4cfd5055289a4b189c
martincavaliers <martincoutts5@gmail.com>
2018-08-11 19:20:02 +0100

    email sub animation added

--- app.js
+++ app.js
@@ -3,17 +3,24 @@
 $(document).ready(function(){
     $(".carousel-text").fadeIn(3000);
 
+    $("#email-sub-header, #email-sub-form").hide();
+
     // Makes sure elements hidden on load
     $(".carousel-caption").children().removeClass("no-js");    
     
-    // Navbar resizing
-    $(document).scroll(function(){
-        if($(document).scrollTop() >= 380){
-            $("nav").css("height", "100px");
-        }else{
-            $("nav").css("height", "180px");  
-        }
-    });
+    // // Navbar resizing
+    // $(document).scroll(function(){
+    //     if($(document).scrollTop() >= 380 && $(document).width() > 993){
+    //         $("nav").css("height", "100px");
+    //     }else{
+    //         $("nav").css("height", "180px");  
+    //     }
+    // });
 
     // -----------Email Subscription---------------
+    $(document).scroll(function(){
+        if($(document).scrollTop() >= 2700){
+            $("#email-sub-header, #email-sub-form").fadeIn(3000);
+        }
+    });
 });
