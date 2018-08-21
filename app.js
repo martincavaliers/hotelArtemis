30a94cee0fcba3aaca28f72f3591c4d426fa1a87
martincavaliers <martincoutts5@gmail.com>
2018-08-21 20:23:45 +0100

    still working on javascript functionality

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 'use strict'
 
-$(document).ready(function(){
+
+$(document).ready(function(){    
     $(".carousel-text").fadeIn(3000);
 
     $("#email-sub-header, #email-sub-form").hide();
 
     // Makes sure elements hidden on load
     $(".carousel-caption").children().removeClass("no-js");    
-    
-    // // Navbar resizing
-    // $(document).scroll(function(){
-    //     if($(document).scrollTop() >= 380 && $(document).width() > 993){
-    //         $("nav").css("height", "100px");
-    //     }else{
-    //         $("nav").css("height", "180px");  
-    //     }
-    // });
 
     // -----------Email Subscription---------------
     
